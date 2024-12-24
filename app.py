9b860b25934270e47ea751465e0715469849fa7a
Otabek <burhonovotabek5@gmail.com>
2024-12-24 10:48:52 +0500

    added

--- app.py
+++ app.py
@@ -44,6 +44,39 @@ def predict():
     # Javobni JSON formatda qaytarish
     return jsonify({'savol': user_input, 'javob': predicted_answer[0]})
 
+# API endpoint: /add_question (yangi savol qo‘shish)
+@app.route('/add_question', methods=['POST'])
+def add_question():
+    # Foydalanuvchidan yangi savol va javobni olish
+    data = request.get_json()  # JSON formatda so'rov
+    new_question = data.get('savol')
+    new_answer = data.get('javob')
+    new_category = data.get('tasnif')
+
+    if not new_question or not new_answer or not new_category:
+        return jsonify({'error': 'Yangi savol, javob yoki tasnif berilmadi!'}), 400
+    
+    # Yangi savol va javobni CSV faylga qo‘shish
+    new_data = pd.DataFrame([[new_question, new_answer, new_category]], columns=['savol', 'javob', 'tasnif'])
+    new_data.to_csv('file.csv', mode='a', header=False, index=False)
+
+    # Modelni qayta o‘rganish (re-train)
+    global X, y, tfidf, model
+    data = pd.read_csv('file.csv')
+    X = data['savol']
+    y = data['tasnif']
+
+    # Train va test ma'lumotlarini yangilash
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # TF-IDF vektorlashtirish
+    X_train_tfidf = tfidf.fit_transform(X_train)
+    X_test_tfidf = tfidf.transform(X_test)
+
+    # Modelni qayta o'rgatish
+    model.fit(X_train_tfidf, y_train)
+
+    return jsonify({'message': 'Yangi savol va javob muvaffaqiyatli qo‘shildi va model yangilandi!'})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render avtomatik port beradi
